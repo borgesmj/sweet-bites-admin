704437e35d9569fe3b5a7a5f77abf309a4b4fd68
borgesmj19 <borgesmj19@gmail.com>
2024-11-30 11:38:06 +0000

    login with google btn

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,42 @@ import Cupones from "./pages/Cupones";
 import Pendientes from "./pages/Pendientes";
 import Completados from "./pages/Completados";
 import Cancelados from "./pages/Cancelados";
-import { Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import { Routes, Route, useLocation } from "react-router";
 import { useApp } from "./Actions/ContextProvider";
 import EditModal from "./components/EditModal";
 import CouponModal from "./components/CouponModal";
 const App = () => {
   const { selectedEditProduct, editModalOpen, couponModalOpen } = useApp();
+
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
   return (
-    <Dashboard>
-      <div className="container ml-56">
-        <Routes>
-          <Route path="/pedidos" element={<Pedidos />}>
-            {/* Rutas anidadas */}
-            <Route index element={<Pendientes />}></Route>
-            <Route path="pendientes" element={<Pendientes />} />
-            <Route path="completados" element={<Completados />} />
-            <Route path="cancelados" element={<Cancelados />} />
-          </Route>
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/galeria" element={<Galeria />} />
-          <Route path="/cupones" element={<Cupones />} />
-        </Routes>
-      </div>
-      {editModalOpen && <EditModal product={selectedEditProduct} />}
-      {couponModalOpen && <CouponModal />}
-    </Dashboard>
+    <>
+      {!isLoginPage && (
+        <Dashboard>
+          <div className="container ml-56">
+            <Routes>
+              <Route path="/pedidos" element={<Pedidos />}>
+                {/* Rutas anidadas */}
+                <Route index element={<Pendientes />}></Route>
+                <Route path="pendientes" element={<Pendientes />} />
+                <Route path="completados" element={<Completados />} />
+                <Route path="cancelados" element={<Cancelados />} />
+              </Route>
+              <Route path="/productos" element={<Productos />} />
+              <Route path="/galeria" element={<Galeria />} />
+              <Route path="/cupones" element={<Cupones />} />
+            </Routes>
+          </div>
+          {editModalOpen && <EditModal product={selectedEditProduct} />}
+          {couponModalOpen && <CouponModal />}
+        </Dashboard>
+      )}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+const Login = () => {
+  return (
+    <div className="w-dvw h-dvh flex flex-row justify-center items-center">
+      <div className="w-1/3 h-full bg-blue-400 flex justify-center items-center px-4">
+        <h3 className="text-2xl text-left text-white w-full">
+          Bienvenida de vuelta
+        </h3>
+      </div>
+      <div className="w-2/3 h-full  flex justify-center items-center">
+        <button
+          type="button"
+          className="login-with-google-btn  bg-no-repeat bg-white border-none rounded-[3px] text-[#757575] text-[14px] font-[500] py-3 pr-4 pl-11 shadow-md transition-all hover:shadow-lg active:bg-[#eeeeee]"
+          onClick={()=>{console.log('login')}}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
